505d2dd8bca8b6af39115e6ecde1e90fdce5f222
Yashvi Agrawal <Tapku>
2023-01-30 08:54:32 +0530

    modul 1

---
+++ 1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+	printf("MY NAME IS\t:-Yashvi\n");
+	printf("MY AGE\t:- 19\n");
+	printf("MY ADDRESS:\t13,shardanagar soc, lane-1,paldi.\n");
+	printf("MY BIRTHDATE:\t 17-01-2004");
+	getch();
+}
---
+++ 2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+	int a;
+	printf("enter value : \n");
+	scanf("%i",&a);
+	printf("Square is: %i",a*a);
+	printf("\nCube is: %i",a*a*a);	
+}
